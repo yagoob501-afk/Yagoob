9c9893431ee59129fcf58130e1faf72affd03b6a
MaDz <000madz000@gmail.com>
2025-09-28 12:03:11 +0300

    adding header + footer to the documentation form page

--- src/pages/forms/project-documentation/1/page.tsx
+++ src/pages/forms/project-documentation/1/page.tsx
@@ -3,6 +3,8 @@ import type { DocumentationData } from "./documentation";
 // import ProjectDocumentationPreview from "./documentation";
 import ProjectDocumentation1Form from "./form";
 import ProjectDocumentationPreview from "./documentation";
+import PrimaryHeader from "@/components/sections/Header/PrimaryHeader";
+import PrimaryFooter from "@/components/sections/Footer/PrimaryFooter";
 // import "react-datepicker/dist/react-datepicker.css";
 
 function ProjectDocumentation1() {
@@ -10,10 +12,15 @@ function ProjectDocumentation1() {
 
 
     return (
-        <main className="container mx-auto grow flex items-center justify-center px-4 py-8">
+        <div className="grow flex flex-col">
+            <PrimaryHeader />
+            <main className="container mx-auto grow flex items-center justify-center px-4 py-8">
 
-            {fullData ? <ProjectDocumentationPreview data={fullData} /> : <ProjectDocumentation1Form onSubmit={setFullData} />}
-        </main>
+                {fullData ? <ProjectDocumentationPreview data={fullData} /> : <ProjectDocumentation1Form onSubmit={setFullData} />}
+            </main>
+
+            <PrimaryFooter />
+        </div>
     );
 }
 
