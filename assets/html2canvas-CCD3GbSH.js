0c8c2ac905da42548066528ffaf0e90707664e2e
MaDz <000madz000@gmail.com>
2025-10-05 10:07:08 +0300

    Updates

--- assets/html2canvas-CCD3GbSH.js
+++ assets/html2canvas-CCD3GbSH.js
@@ -1,4 +1,4 @@
-import{n as e}from"./index-yj0jCJK1.js";var t=e(((exports,t)=>{
+import{n as e}from"./index-C36V5HMf.js";var t=e(((exports,t)=>{
 /*!
 * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
 * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
