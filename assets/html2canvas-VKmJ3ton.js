91dd5a80326af3ef5b2fd1784825bf9d2a5f81dd
MaDz <000madz000@gmail.com>
2025-10-14 20:54:18 +0300

    Updates

--- assets/html2canvas-VKmJ3ton.js
+++ assets/html2canvas-VKmJ3ton.js
@@ -1,4 +1,4 @@
-import{n as e}from"./index-QX0eBJ3L.js";var t=e(((exports,t)=>{
+import{n as e}from"./index-CvjUmXjD.js";var t=e(((exports,t)=>{
 /*!
 * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
 * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
