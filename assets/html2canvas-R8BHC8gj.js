f76ba29063a61bd12ce530fedbfa275eae4748bb
MaDz <000madz000@gmail.com>
2025-10-15 10:14:35 +0300

    Updates

--- assets/html2canvas-R8BHC8gj.js
+++ assets/html2canvas-R8BHC8gj.js
@@ -1,4 +1,4 @@
-import{n as e}from"./index-DFOm_yiJ.js";var t=e(((exports,t)=>{
+import{n as e}from"./index-DM-sC1DQ.js";var t=e(((exports,t)=>{
 /*!
 * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
 * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
