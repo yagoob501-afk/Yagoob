6bf77fab1c16c7050ac4cfb8ae771f0c77b3e3ce
MaDz <000madz000@gmail.com>
2025-10-18 17:14:25 +0300

    adding pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => {
     // Load env variables based on mode
@@ -12,6 +13,23 @@ export default defineConfig(({ mode }) => {
         base: env.VITE_PUBLIC_APP_BASE || '/',
         plugins: [
             react(),
+            VitePWA({
+                manifest: {
+                    "name": "yagoob forms",
+                    "short_name": "yagoob forms",
+                    "start_url": "/Yagoob/",
+                    "display": "standalone",
+                    "background_color": "#ffffff",
+                    "theme_color": "#000000",
+                    "icons": [
+                        {
+                            "src": "/Yagoob/logo-main.png",
+                            "sizes": "533x350",
+                            "type": "image/png"
+                        }
+                    ]
+                }
+            }),
             tailwindcss(),
             tsconfigPaths({
                 projects: ['./tsconfig.app.json']
