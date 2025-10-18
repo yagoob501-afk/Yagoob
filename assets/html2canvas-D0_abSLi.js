6173234931df99edc355f8e66418d6a7b0e9f5ea
MaDz <000madz000@gmail.com>
2025-10-18 22:24:53 +0300

    Updates

--- assets/html2canvas-D0_abSLi.js
+++ assets/html2canvas-D0_abSLi.js
@@ -1,4 +1,4 @@
-import{n as e}from"./index-DjLbxAFc.js";var t=e(((exports,t)=>{
+import{n as e}from"./index-BunKMhWL.js";var t=e(((exports,t)=>{
 /*!
 * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
 * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
