da1795bfd1ee394bcc82446e6ac73413c0e1ae45
MaDz <000madz000@gmail.com>
2025-10-20 01:15:45 +0300

    remove the public app base from env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,47 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => {
-    // Load env variables based on mode
-
+    // Load environment variables
     const env = loadEnv(mode, process.cwd(), '')
 
+    // Remove trailing slash if exists (just to be safe)
+    const base = (env.VITE_PUBLIC_APP_BASE || '/').replace(/\/$/, '')
+
     return {
-        base: env.VITE_PUBLIC_APP_BASE || '/',
+        base, // this will be like: /Yagoob
         plugins: [
             react(),
             VitePWA({
                 manifest: {
-                    "name": "yagoob forms",
-                    "short_name": "yagoob forms",
-                    "start_url": "/Yagoob/",
-                    "display": "standalone",
-                    "background_color": "#ffffff",
-                    "theme_color": "#000000",
-                    "icons": [
+                    name: 'yagoob forms',
+                    short_name: 'yagoob forms',
+                    start_url: `${base}/`,
+                    display: 'standalone',
+                    background_color: '#ffffff',
+                    theme_color: '#000000',
+                    icons: [
                         {
-                            "src": "/Yagoob/logo-main.png",
-                            "sizes": "533x350",
-                            "type": "image/png"
-                        }
+                            src: `${base}/logo-main.png`,
+                            sizes: '533x350',
+                            type: 'image/png',
+                        },
                     ],
                     screenshots: [
                         {
-                            "src": "/Yagoob/manifest/1.jpeg",
-                            sizes: '1788x1444'
-                        }
-                    ]
-                }
+                            src: `${base}/manifest/1.jpeg`,
+                            sizes: '1788x1444',
+                        },
+                    ],
+                },
             }),
             tailwindcss(),
             tsconfigPaths({
-                projects: ['./tsconfig.app.json']
-            })
+                projects: ['./tsconfig.app.json'],
+            }),
         ],
     }
 })
