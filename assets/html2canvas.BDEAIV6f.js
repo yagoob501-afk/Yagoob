fe9b45b9a583fa54b0e46f808cdc4b7f81602cf9
MaDz <000madz000@gmail.com>
2026-03-14 16:11:21 +0200

    Updates

--- assets/html2canvas.BDEAIV6f.js
+++ assets/html2canvas.BDEAIV6f.js
@@ -1,4 +1,4 @@
-import{n as e}from"./index.Dqw9mM_q.js";var t=e(((exports,t)=>{
+import{n as e}from"./index.D1fRFU4p.js";var t=e(((exports,t)=>{
 /*!
 * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
 * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
