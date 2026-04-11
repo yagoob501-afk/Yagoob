2c5d713d0414bde733f59c3bd3b3edd30bbc5d7a
MaDz <000madz000@gmail.com>
2026-04-11 07:59:53 +0200

    Updates

--- assets/html2canvas.DUePnAVv.js
+++ assets/html2canvas.DUePnAVv.js
@@ -1,4 +1,4 @@
-import{n as e}from"./index.WJ8y1riY.js";var t=e(((exports,t)=>{
+import{n as e}from"./index.CWUc0lC8.js";var t=e(((exports,t)=>{
 /*!
 * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
 * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
